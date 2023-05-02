4ccc00a935616bfb232870a23f03e1abad038d53
Kirill Golygin <kirik3007@gmail.com>
2023-05-02 17:11:17 +0300

    add types

--- src/js/modules/showPic.ts
+++ src/js/modules/showPic.ts
@@ -1,23 +1,23 @@
-export const showPic = (blocksSelector: string) => {
-    const blocks = document.querySelectorAll(blocksSelector); 
+export const showPic = (blocksSelector: string): void => {
+    const blocks: NodeListOf<HTMLDivElement> = document.querySelectorAll(blocksSelector); 
 
-    blocks.forEach((block: any, i) => {
+    blocks.forEach((block: HTMLDivElement, i) => {
+        const paragraphs: NodeListOf<HTMLParagraphElement> = block.querySelectorAll('p:not(.sizes-hit)');
+        const pic: HTMLPictureElement|null = block.querySelector('img');
 
         block.addEventListener('mouseenter', () => {
-            block.querySelectorAll('p:not(.sizes-hit)').forEach((p:any) => {
+            paragraphs.forEach((p:HTMLParagraphElement) => {
                 p.style.display = 'none';
             })
 
-            const pic = block.querySelector('img');
             pic?.setAttribute('src', `./src/assets/img/sizes-${i+1}-1.png`);
         });
 
         block.addEventListener('mouseleave', () => {
-            block.querySelectorAll('p').forEach((p:any) => {
+            paragraphs.forEach((p:HTMLParagraphElement) => {
                 p.style.display = 'block';
             })
 
-            const pic = block.querySelector('img');
             pic?.setAttribute('src', `./src/assets/img/sizes-${i+1}.png`);
         });
     });
