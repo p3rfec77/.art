f159fc498d84dd42f2045e6fd63e4d1e6bf6de76
Kirill Golygin <kirik3007@gmail.com>
2023-05-03 19:01:59 +0300

    remake using maxHeight

--- src/js/modules/accordion.ts
+++ src/js/modules/accordion.ts
@@ -2,24 +2,14 @@ export const accordion = (headersSelector: string, contentSelector: string) => {
     const headers: NodeListOf<HTMLElement> = document.querySelectorAll(headersSelector);
     const content: NodeListOf<HTMLElement> = document.querySelectorAll(contentSelector);
 
-    content.forEach((block:HTMLElement) => {
-        block.style.display = 'none';
-    });  
-
-    headers.forEach((header, i) => {
+    headers.forEach((header: HTMLElement, i: number) => {
         header.addEventListener('click', (e) => {
             const target  = e.target as Node;
             if(target === header || target.parentNode === header) {
-                headers.forEach((header) => {
-                    header.classList.remove('ui-accordion-header-active');
-                });
-                header.classList.add('ui-accordion-header-active');
 
-                content.forEach((block:HTMLElement) => {
-                    block.style.display = 'none';
-                    block.classList.add('animated', 'fadeInDown');
-                }); 
-                content[i].style.display = 'block';
+                header.classList.toggle('ui-accordion-header-active');
+
+                content[i].style.maxHeight ? content[i].style.maxHeight = '' : content[i].style.maxHeight = content[i].scrollHeight + 'px';
             }
         });
     });
