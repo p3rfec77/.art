49ac94343e25025bbdf85569dab59ed6c2a9edda
Kirill Golygin <kirik3007@gmail.com>
2023-05-04 17:37:39 +0300

    change method of finding window width

--- src/ts/modules/burger.ts
+++ src/ts/modules/burger.ts
@@ -7,17 +7,17 @@ export const burger = (burgerSelector: string, menuSelector: string): void => {
 
     menu.style.display = 'none';
 
+    const tabletWidth: number = 993;
+
     burger?.addEventListener('click', (e) => {
         const target = e.target as Node;
-        const tabletWidth: number = 993;
          if (target === burger || target.parentNode === burger) {
-             menu.style.display = (menu.style.display === 'none' && window.screen.availWidth < tabletWidth) ? 'block' : 'none';
+             menu.style.display = (menu.style.display === 'none' &&  window.innerWidth < tabletWidth) ? 'block' : 'none';
         }
     });
 
     window.addEventListener('resize', () => {
-        const BREAKPOINT: number = 992;
-        if(window.screen.availWidth >= BREAKPOINT) {
+        if( window.innerWidth >= tabletWidth) {
             menu.style.display = 'none';
         }
     });
